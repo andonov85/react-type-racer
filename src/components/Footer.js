7f2eddff9a71c12be597543f7f111f878cef35c8
Andon Andonov <andon.andonov85@gmail.com>
2021-02-24 22:22:47 +0200

    Added Google log out hook

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,30 +1,24 @@
 import { useHistory } from 'react-router-dom';
-import GoogleLogout from 'react-google-login';
+import { useGoogleLogout } from 'react-google-login'
 
 import { useAuth } from './Auth';
 
 function Footer({ children }) {
     let history = useHistory();
     let auth = useAuth();
-
-    const logOut = () => {
-        auth.signout(() => history.push("/"));
-    }
+    const { signOut } = useGoogleLogout({
+        onLogoutSuccess: () => {
+            auth.signout(() => history.push("/"));
+        },
+        onFailure: () => console.log(`Failure: Can't retrieve googleUser!`),
+        clientId: "815652919811-04teroae4aok8u52359jdr99fg4hdbk1.apps.googleusercontent.com",
+        cookiePolicy: 'single_host_origin',
+        uxMode: 'redirect',
+      })
 
     return (
         <footer className="rtr-footer--container">
-            <a className="google-logout" href="/" onClick={logOut}>Log out</a>
-            {/* <GoogleLogout
-                className=""
-                clientId="815652919811-04teroae4aok8u52359jdr99fg4hdbk1.apps.googleusercontent.com"
-                tag={'a'}
-                // render={renderProps => (
-                //     <a className="google-log-out" onClick={renderProps.onClick} disabled={renderProps.disabled}>Log Out</a>
-                // )}
-                buttonText="Log Out"
-                onLogoutSuccess={logout}
-                isSignedIn={false}
-            /> */}
+            <a className="google-logout" onClick={signOut}>Log out</a>
         </footer>
     );
 }
